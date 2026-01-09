41087903d58fc747a60d6ca5834df9a7ab00ef0b
Kommiri <amaranath.kommiri@accenture.com>
2026-01-09 12:44:10 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,8 @@
+local {
+    common_tags = {
+        Project = var.project_name
+        Environment = var.environment
+        Terraform = true
+    }
+    common_name_suffix = "${var.project_name}-${var.environment}"
+}
